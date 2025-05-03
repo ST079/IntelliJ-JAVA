14cd485e25786889b54eb1f8a273bf0e8d0b3c95
Sujan Tamang <suzanyba079@gmail.com>
2025-05-03 08:20:30 +0545

    constructors

--- src/com/basics/constructors.java
+++ src/com/basics/constructors.java
@@ -1,5 +1,43 @@
 package com.basics;
+class MyEmp{
+    private int id;
+    private String name;
+    float salary;
 
+    //constructors
+    public MyEmp(int a,String b){
+        id = a;
+        name = b;
+    }
+
+    MyEmp(){
+        salary = 10;
+    }
+    public void setName(String n){ //setter
+        name = n;
+    }
+    public String getName(){//getter
+        return name;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+    public float getSalary(){
+       return salary;
+    }
+}
 public class constructors {
-
+ public static void main(String[] args) {
+     //using the constructors
+//  MyEmp kusum = new MyEmp(1,"Kusum Darlami");
+//  MyEmp sujan = new MyEmp(2,"Sujan Tamang");
+     MyEmp kusum = new MyEmp();
+  kusum.setId(1);
+  kusum.setName("Jack");
+     System.out.println(kusum.getName() + " = " + kusum.getId() + " " + kusum.getSalary());
+//     System.out.println(sujan.getName() + " = " + sujan.getId());
+ }
 }
